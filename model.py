Fixed the bug to evaluate the baseline model. The average F1-score is 0.117.

model.py
@@ -1,3 +1,4 @@
+import ast
 import concurrent.futures
 import json
 import jsonref
@@ -5,6 +6,7 @@ import math
 import networkx as nx
 import numpy as np
 import os
+import subprocess
 import sys
 import torch
 import torch.nn as nn
@@ -12,6 +14,8 @@ import tqdm
 import wandb
 
 from adapters import AdapterTrainer, AutoAdapterModel
+from collections import defaultdict
+from concurrent.futures import ThreadPoolExecutor
 from copy import copy, deepcopy
 from torch.optim import AdamW
 from transformers import AutoTokenizer, get_scheduler, TrainingArguments, EvalPrediction
@@ -25,7 +29,7 @@ PATH = "./adapter-model"
 ADAPTER_NAME = "frequent_patterns"
 SCHEMA_FOLDER = "processed_schemas"
 JSON_FOLDER = "processed_jsons"
-BATCH_SIZE = 8
+BATCH_SIZE = 32
 JSON_SUBSCHEMA_KEYWORDS = {"allOf", "oneOf", "anyOf", "not"}
 
 # https://stackoverflow.com/a/73704579
@@ -199,7 +203,7 @@ def train_model(train_df, test_df):
     """
 
     accumulation_steps = 4
-    learning_rate = 1e-6
+    learning_rate = 2e-6
     num_epochs = 100
 
     # Start a new wandb run to track this script
@@ -410,13 +414,9 @@ def find_pairs_with_common_properties(df):
             schema = json.loads(df.at[i, "schema"])
             properties = schema.get("properties", {})
             properties_list.append(set(properties.keys()))
-        except (json.JSONDecodeError) as e:
+        except (json.JSONDecodeError, KeyError) as e:
             print(f"Skipping row {i} due to error: {e}")
             properties_list.append(set()) 
-        except Exception as e:
-            print(f"Skipping row {i} due to error: {e}")
-            properties_list.append(set())
-        
 
     # Compare each pair of schemas
     for i in range(len(df)):
@@ -434,7 +434,15 @@ def find_pairs_with_common_properties(df):
 
 
 def extract_properties(schema):
-    """Extract properties from a JSON schema."""
+    """
+    Extracts the top-level properties from a JSON schema.
+    
+    Args:
+        schema (dict): The JSON schema.
+
+    Returns:
+        dict: A dictionary containing the top-level properties.
+    """
     if "properties" in schema:
         return schema["properties"]
     return {}
@@ -457,7 +465,7 @@ def order_properties_by_commonality(schema1, schema2):
     properties2 = extract_properties(schema2)
 
     # Find common properties
-    common_properties = set(properties1.keys()).intersection(set(properties2.keys()))
+    common_properties = set(properties1.keys()) & set(properties2.keys())
 
     # Order properties by commonality
     ordered_properties1 = {prop: properties1[prop] for prop in common_properties}
@@ -575,13 +583,13 @@ def process_pairs(pairs, df, model, device, tokenizer):
     return edges
 
 
-def build_definition_graph(df, model, device, tokenizer):
+def build_definition_graph(df, m, device, tokenizer):
     """
     Build a definition graph from tokenized schema pairs using the given model, without batching.
 
     Args:
         df (pd.DataFrame): A DataFrame containing paths and their tokenized schemas.
-        model (PreTrainedModel): The model used for predicting connections.
+        m (PreTrainedModel): The model used for predicting connections.
         device (torch.device): The device (CPU or GPU) to run the model on.
         tokenizer (PreTrainedTokenizer): The tokenizer used for processing schemas.
 
@@ -592,7 +600,7 @@ def build_definition_graph(df, model, device, tokenizer):
     pairs = list(find_pairs_with_common_properties(df))
     
     with tqdm.tqdm(total=len(pairs), desc="Processing pairs", position=0, leave=True) as pbar:
-        edges = process_pairs(pairs, df, model, device, tokenizer)
+        edges = process_pairs(pairs, df, m, device, tokenizer)
 
         # Add edges to the graph
         for edge in edges:
@@ -613,28 +621,30 @@ def find_definitions_from_graph(graph):
     Returns:
         List[List]: A list of lists, each containing nodes representing a definition.
     """
-    # Get all the cliques from the graph and sort them based on their lengths in ascending order
+    # Get all the cliques from the graph and sort them based on their lengths in descending order
     cliques = list(nx.algorithms.find_cliques(graph))
-    cliques.sort(key=lambda a: len(a))
+    cliques.sort(key=lambda a: len(a), reverse=True)
     
     # Make sure a path only appears in one definition
     processed_cliques = []
     while cliques:
-        # Remove the last clique
-        largest_clique = cliques.pop()
+        # Remove the largest clique
+        largest_clique = cliques.pop(0)
         # Create a set of vertices in the largest clique
-        clique_set = set(largest_clique)
+        clique_set = sorted(set(largest_clique))
+
         # Filter out vertices from other cliques that are in the largest clique
         filtered_cliques = []
         for clique in cliques:
             filtered_clique = [c for c in clique if c not in clique_set]
             if len(filtered_clique) > 1:
                 filtered_cliques.append(filtered_clique)
+        
         processed_cliques.append(list(clique_set))
         cliques = filtered_cliques
 
     return processed_cliques
-
+ 
 
 def calc_jaccard_index(actual_cluster, predicted_cluster):
     """Measure the similarity between actual and predicted clusters
@@ -717,57 +727,56 @@ def evaluate_single_schema(schema, device, test_ground_truth):
     Args:
         schema (str): The schema filename to be evaluated.
         device (torch.device): The device on which to run the model.
-        tokenizer (PreTrainedTokenizer): The tokenizer for schema tokenization.
         test_ground_truth (dict): Ground truth clusters for test schemas.
 
     Returns:
-        tuple: Contains schema name, precision, recall, F1 score, actual clusters, and predicted clusters.
+        tuple: Contains schema name, precision, recall, F1 score, sorted actual clusters, and sorted predicted clusters.
         None: If the schema could not be processed.
     """
-    
+
     # Load the model and tokenizer
     m, tokenizer = load_model_and_adapter()
-    m.eval()
-    m.to(device)
 
-    filtered_df, frequent_ref_defn_paths, schema_name, failure_flags = process_data.process_schema(schema)
+    # Enable multiple GPU usage with DataParallel
+    if torch.cuda.device_count() > 1:
+        print(f"Using {torch.cuda.device_count()} GPUs for model parallelism.")
+        m = nn.DataParallel(m)
+
+    m.to(device)
+    m.eval()
+
+    filtered_df, frequent_ref_defn_paths, schema_name, failure_flags = process_data.process_schema(schema, "")
         
     if filtered_df is not None and frequent_ref_defn_paths is not None:
-        # Filter paths with schema
-        filtered_df = remove_additional_properties(filtered_df, schema)
-        filtered_df[["path", "schema"]].to_csv(f"filtered_df_{schema_name}.csv", index=False)
-        if filtered_df.empty:
-            return None
-        
-        print(f"Schema: {schema}, Number of paths: {len(filtered_df)}")
-        
         # Build a definition graph
         graph = build_definition_graph(filtered_df, m, device, tokenizer)
-        print(f"Number of edges: {len(graph.edges)}")
         
-        # Predict clusters
-        predicted_clusters = find_definitions_from_graph(graph)
-        print(f"Number of predicted clusters: {len(predicted_clusters)}")
+        # Predict clusters and sort each predicted cluster alphabetically
+        predicted_clusters = [sorted(cluster) for cluster in find_definitions_from_graph(graph)]
 
-        # Get the ground truth clusters
+        # Get the ground truth clusters and sort each actual cluster alphabetically
         ground_truth_dict = test_ground_truth.get(schema_name, {})
-        actual_clusters = [[tuple(inner_list) for inner_list in outer_list] for outer_list in ground_truth_dict.values()]
+        actual_clusters = [[sorted(tuple(inner_list)) for inner_list in outer_list] for outer_list in ground_truth_dict.values()]
 
+        print(f"Schema: {schema_name}")
+        print(f"Actual clusters: {actual_clusters}")
+        print(f"Predicted clusters: {predicted_clusters}")
         # Calculate the precision, recall, and F1-score
         precision, recall, f1_score = calc_scores(actual_clusters, predicted_clusters)
 
-        # Return evaluation metrics and clusters for the schema
+        # Return evaluation metrics and sorted clusters for the schema
         return schema, precision, recall, f1_score, actual_clusters, predicted_clusters
         
     return None
 
 
-def evaluate_data(test_ground_truth):
+def evaluate_data(test_ground_truth, output_file="evaluation_results.json"):
     """
-    Evaluate the model on the entire test data using multiple CPUs.
+    Evaluate the model on the entire test data using multiple CPUs and store results in a JSON file.
 
     Args:
         test_ground_truth (dict): Dictionary containing ground truth information for test data.
+        output_file (str): Path to the JSON file to store the evaluation results.
     """
 
     # Set device
@@ -776,31 +785,213 @@ def evaluate_data(test_ground_truth):
     # Get the test schemas
     test_schemas = list(test_ground_truth.keys())
     f1_scores = []
+    results = []
 
     # Process schemas in parallel using ProcessPoolExecutor
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        futures = {executor.submit(evaluate_single_schema, schema, device, test_ground_truth): schema for schema in test_schemas}
+    with ThreadPoolExecutor() as executor:
+        futures = {
+            executor.submit(evaluate_single_schema, schema, device, test_ground_truth): schema 
+            for schema in test_schemas
+        }
 
         for future in tqdm.tqdm(concurrent.futures.as_completed(futures), total=len(futures), position=1):
             result = future.result()
             if result is not None:
-                
                 schema, precision, recall, f1_score, actual_clusters, predicted_clusters = result
                 f1_scores.append(f1_score)
 
-                # Print the evaluation metrics
-                print(f"Schema: {schema}, Precision: {precision}, Recall: {recall}, F1-score: {f1_score}")
+                # Collect results for each schema
+                results.append({
+                    "schema": schema,
+                    "precision": precision,
+                    "recall": recall,
+                    "f1_score": f1_score,
+                    "actual_clusters": actual_clusters,
+                    "predicted_clusters": predicted_clusters
+                })
 
-                # Print the actual and predicted clusters
-                print("Actual clusters")
-                for actual_cluster in actual_clusters:
-                    print(actual_cluster)
-
-                print("\nPredicted clusters:")
-                for predicted_cluster in predicted_clusters:
-                    print(predicted_cluster)
-                print("-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n")
-                
-    # Print the average F1-score
+    # Calculate average F1-score and add it to results
     if f1_scores:
-        print("Average F1-score:", sum(f1_scores) / len(f1_scores))
+        avg_f1_score = sum(f1_scores) / len(f1_scores)
+        results.append({"average_f1_score": avg_f1_score})
+
+    # Write results to JSON file
+    with open(output_file, "w") as json_file:
+        json.dump(results, json_file, indent=4)
+
+    print(f"Results saved to {output_file}")
+
+
+def group_paths(df, test_ground_truth, min_common_keys=2, output_file="evaluation_results_baseline_model.json"):
+    """
+    Group paths that have at least a specified number of distinct nested keys in common per filename and evaluate.
+
+    Args:
+        df (pd.DataFrame): DataFrame containing columns "path", "distinct_nested_keys", and "filename".
+        test_ground_truth (dict): Dictionary containing ground truth clusters for test data.
+        min_common_keys (int, optional): Minimum number of distinct nested keys required for paths to be grouped together. Default is 2.
+        output_file (str, optional): Path to the output JSON file. Default is "evaluation_results_baseline_model.json".
+
+    Returns:
+        None
+    """
+    all_results = []  # To accumulate results for each filename
+    f1_scores = []  # To store F1 scores for each schema
+
+    # Iterate through each filename group
+    for filename, group in tqdm.tqdm(df.groupby("filename"), position=1, leave=False, total=len(df["filename"].unique()), desc="Grouping paths"):
+        paths = group["path"].tolist()
+        distinct_keys = group["distinct_nested_keys"].tolist()
+
+        # Dictionary to track paths based on shared keys
+        group_dict = defaultdict(set)
+
+        # Process each path and its associated keys
+        for path, keys in zip(paths, distinct_keys):
+            # Convert the strings to actual sets
+            path = ast.literal_eval(path)
+            keys = frozenset(ast.literal_eval(keys))
+            
+            added_to_group = False
+
+            # Only start checking if there are groups in group_dict
+            if group_dict:
+                # Attempt to find or create a group for the current path
+                for existing_keys in list(group_dict.keys()):
+                    # Check if this path has sufficient keys in common with an existing group
+                    if len(keys & existing_keys) >= min_common_keys:
+                        # Add the path to the existing group
+                        group_dict[existing_keys].append(path)
+                        added_to_group = True
+                        break
+
+            # If no existing group was found, add a new group for this path
+            if not added_to_group:
+                group_dict[keys] = [path]
+        
+        # Filter groups to keep only valid ones with more than one path
+        predicted_clusters = [sorted(group) for group in group_dict.values() if len(group) > 1]
+        predicted_clusters = sorted(predicted_clusters)
+
+
+        # Get the actual clusters for the current filename
+        defn_paths_dict = test_ground_truth.get(filename, {})
+        actual_clusters = [sorted([tuple(path) for path in cluster]) for cluster in sorted(defn_paths_dict.values())]
+
+        # Evaluate the predicted clusters against the ground truth
+        precision, recall, f1_score = calc_scores(actual_clusters, predicted_clusters)
+
+        # Store individual F1 scores for average calculation later
+        f1_scores.append(f1_score)
+
+        # Collect results for the current schema
+        results = {
+            "schema": filename,
+            "precision": precision,
+            "recall": recall,
+            "f1_score": f1_score,
+            "actual_clusters": actual_clusters,
+            "predicted_clusters": predicted_clusters
+        }
+        all_results.append(results)
+
+    # Calculate average F1 score if there are any scores
+    if f1_scores:
+        avg_f1_score = sum(f1_scores) / len(f1_scores)
+        all_results.append({"average_f1_score": avg_f1_score})
+
+    # Write all results to the specified JSON file
+    with open(output_file, "w") as json_file:
+        json.dump(all_results, json_file, indent=4)
+
+
+    
+
+def get_definition(ref, schema):
+    """
+    Retrieve the object definition from a JSON schema based on a `$ref` reference string.
+
+    Args:
+        ref (str): The `$ref` reference string pointing to a specific definition within the schema.
+        schema (dict): The full JSON schema containing definitions.
+
+    Returns:
+        dict: The dereferenced object from the schema corresponding to the `$ref` string.
+
+    Raises:
+        KeyError: If the `$ref` path does not exist in the schema.
+    """
+    try:
+        if ref.startswith("#/definitions/"):
+            parts = ref.split("/")
+            definition = schema["definitions"]
+            for part in parts[2:]:
+                if part:
+                    definition = definition[part]
+            return definition
+
+        elif ref.startswith("$defs/"):
+            parts = ref.split("/")
+            definition = schema["$defs"]
+            for part in parts[1:]:
+                if part:
+                    definition = definition[part]
+            return definition
+
+    except KeyError as e:
+        raise KeyError(f"Reference {ref} could not be resolved in schema.") from e
+
+    # Unhandled reference format
+    return None
+
+
+def dereference_schema(schema_path, excluded_definitions):
+
+     # Convert the excluded_definitions set to a string or JSON representation
+    excluded_definitions_str = json.dumps(list(excluded_definitions))
+
+    # Build the command to call the Node.js script with the file paths
+    command = ["node", "dereference_schema.js", schema_path, excluded_definitions_str]
+
+    # Run the Node.js script and capture the output
+    result = subprocess.run(command, capture_output=True, text=True)
+
+    # Handle the output and extract the size in kilobytes
+    if result.returncode == 0:
+        output = result.stdout.strip()
+        if output: 
+            return output
+        else:
+            print("Error: Node.js script returned an empty output")
+            return None
+    else:
+        print(f"Error executing Node.js script: {result.stderr}")
+        return None
+
+
+def get_dereferenced_schema_size():
+    """
+    Calls the Node.js script to get the dereferenced schema size while excluding specified definitions.
+
+    Args:
+        schema (dict): The schema to be dereferenced.
+        excluded_definitions (list): List of definitions to exclude (in the format #/definitions/DefinitionName).
+
+    Returns:
+        float: The size of the dereferenced schema in kilobytes.
+    """
+    try:
+        with open("test_ground_truth.json", 'r') as json_file:
+            test_ground_truth_list = [json.loads(line) for line in json_file]
+
+    except json.JSONDecodeError as e:
+        print(f"Error loading JSON file: {e}")
+        return
+
+    for test_ground_truth in test_ground_truth_list:
+        for schema_name, ground_truth in test_ground_truth.items():
+            ref_definitions = list(ground_truth.keys())
+            schema_path = os.path.join(SCHEMA_FOLDER, schema_name)
+            print(dereference_schema(schema_path, ref_definitions))
+
+                